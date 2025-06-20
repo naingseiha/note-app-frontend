c8846b4e9d88b877fe0b8513fca40cf45d29207e
Seiha <naingseiha@gmail.com>
2025-06-20 22:07:52 +0200

    Update package.json

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/note-app-frontend/", // Add this line with your repo name
   plugins: [react()],
 });
